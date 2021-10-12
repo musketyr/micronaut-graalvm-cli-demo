0daf001e234011bfb964dcde6b680255b9aca7af
Vladimir Orany <vladimir@orany.cz>
2021-10-12 08:56:32 +0200

    echo args

--- src/main/java/cz/orany/MicronautGraalvmCliDemoCommand.java
+++ src/main/java/cz/orany/MicronautGraalvmCliDemoCommand.java
@@ -8,14 +8,17 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "micronaut-graalvm-cli-demo", description = "...",
-        mixinStandardHelpOptions = true)
+import java.util.Arrays;
+
+@Command(name = "micronaut-graalvm-cli-demo", description = "...", mixinStandardHelpOptions = true)
 public class MicronautGraalvmCliDemoCommand implements Runnable {
 
     @Option(names = {"-v", "--verbose"}, description = "...")
     boolean verbose;
 
     public static void main(String[] args) throws Exception {
+        System.out.print("Got arguments: ");
+        System.out.println(Arrays.toString(args));
         PicocliRunner.run(MicronautGraalvmCliDemoCommand.class, args);
     }
 
