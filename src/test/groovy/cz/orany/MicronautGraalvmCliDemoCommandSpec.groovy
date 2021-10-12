a229a09850fba82fc75f976db2c24475761a5fe8
Vladimir Orany <vladimir@orany.cz>
2021-10-12 04:52:39 +0000

    Initial commit

---
+++ src/test/groovy/cz/orany/MicronautGraalvmCliDemoCommandSpec.groovy
@@ -0,0 +1,30 @@
+package cz.orany
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
+
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class MicronautGraalvmCliDemoCommandSpec extends Specification {
+
+    @Shared @AutoCleanup ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
+
+    void "test micronaut-graalvm-cli-demo with command line option"() {
+        given:
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()
+        System.setOut(new PrintStream(baos))
+
+        String[] args = ['-v'] as String[]
+        PicocliRunner.run(MicronautGraalvmCliDemoCommand, ctx, args)
+
+        expect:
+        baos.toString().contains('Hi!')
+    }
+}
+
